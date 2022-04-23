d1b3aca3bc5d61e214037ab74cebb4afcb1b3fc7
JimKinyua <jimkinyua25@gmail.com>
2022-04-23 03:07:01 +0300

    Refactor: Ability To Reuse functions

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const csvData =[];
 const res = 0;
 var groupedResult =[] ;
 const groupedBalance = {};
-const groupedApiResult = {};
+let arrayWithUSDValue = [];
 
 parameters = yargs.argv;
 // console.log(parameters);
@@ -21,6 +21,22 @@ const apiVariables ={
 
 if(parameters.date && parameters.token){// Both Params provided
     console.log('both provided');
+    inputStream.pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
+    .on('data', (row)=> {
+        csvData.push(row);
+    })
+    .on('end', function () {
+
+
+        return CalculateUSDValueForEachSymbol(
+                    CalCulateBalanceForEachToken(
+                        GroupArrayAccordingToTokenAndDate(csvData,  parameters.date,  parameters.token)
+                    )
+                );
+
+    });
+
+
 }else if(parameters.token){ //Token Provided
 
     inputStream.pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
@@ -29,9 +45,11 @@ if(parameters.date && parameters.token){// Both Params provided
     })
     .on('end', function () {
 
-        GroupArrayAccordingToToken(csvData, parameters.token);
-        CalCulateBalanceForEachToken(groupedResult);
-        CalculateUSDValueForEachSymbol(groupedBalance);
+      return CalculateUSDValueForEachSymbol( 
+                CalCulateBalanceForEachToken(
+                    GroupArrayAccordingToToken(csvData, parameters.token)
+                ) 
+            );
 
     });
 
@@ -44,9 +62,11 @@ if(parameters.date && parameters.token){// Both Params provided
     })
     .on('end', function () {
         //ToDo: Check date Format to be valid
-        GroupArrayAccordingToDate(csvData, parameters.date);
-        CalCulateBalanceForEachToken(groupedResult);
-        CalculateUSDValueForEachSymbol(groupedBalance);
+       return CalculateUSDValueForEachSymbol(
+                CalCulateBalanceForEachToken(
+                    GroupArrayAccordingToDate(csvData, parameters.date)
+                )
+            );
 
     });
 
@@ -57,68 +77,106 @@ if(parameters.date && parameters.token){// Both Params provided
             csvData.push(row);
         })
         .on('end', function () {
-            GroupArrayAccordingToToken(csvData);
-            CalCulateBalanceForEachToken(groupedResult);
-            CalculateUSDValueForEachSymbol(groupedBalance);
+
+            return CalculateUSDValueForEachSymbol(
+                        CalCulateBalanceForEachToken(
+                            GroupArrayAccordingToToken(csvData)
+                        )
+                    );
         });
 
 }
 
 
 
-function CalculateUSDValueForEachSymbol(arrayWithBalance) {
-    for (let Gname in arrayWithBalance) {
-        axios.get('https://min-api.cryptocompare.com/data/price?fsym=' + Gname + '&tsyms=USD&api_key=a437b4c681a49c9b45382a38c319ed0a795b9180041154d9dc5702e534e72107')
-            .then(response => {
-                groupedApiResult[Gname] = {
-                    LastestBalance: ((arrayWithBalance[Gname].Balance * response.data.USD))
-                };
-                console.log(groupedApiResult);
-            })
-            .catch(error => {
-                console.log(error);
-            });   
-    }
+function CalculateUSDValueForEachSymbol(objectWithBalance) {
 
-    if(arrayWithBalance){// Check if array is empty
-        for (let tokenSymbol in  arrayWithBalance){
+    if(objectWithBalance){// Check if array is empty
+
+        for (let tokenSymbol in  objectWithBalance){
+            
             const url = `https://min-api.cryptocompare.com/data/price?fsym=${tokenSymbol}&tsyms=USD&api_key=${apiVariables.token}`;
-            if(groupedBalance[tokenSymbol].Balance){
+            if(objectWithBalance[tokenSymbol].Balance){
                 axios.get(url)
                 .then(response => {
-                     groupedApiResult[tokenSymbol] = {
+
+                    arrayWithUSDValue[tokenSymbol] = {
                         LastestBalance: ((groupedBalance[tokenSymbol].Balance * response.data.USD))
                     };
-                    // console.log(groupedApiResult);
-    
+
+                    console.log(arrayWithUSDValue)
+
                 })
                 .catch(error => {
                     console.log(error);
                 });
-            } else{
-                console.log('Token Not Found')
-            }   
+            }else{
+                continue
+            }
+
         }
+
+
     }
+
+    
+
+
 }
 
 function CalCulateBalanceForEachToken(GroupedArray) {
     for (let tokenSymbol in GroupedArray) {
-        const accumulated = groupedResult[tokenSymbol].reduce((acc, curr) => {
-            acc[curr[1]] += curr[3];
+        // console.log(GroupedArray)
+        const accumulated = GroupedArray[tokenSymbol].reduce((acc, curr) => {
+            if(curr[1] == 'DEPOSIT'){
+                acc[curr[1]] += curr[3]; 
+            }
+            if(curr[1] == 'WITHDRAWAL'){
+                acc[curr[1]] += curr[3]; 
+            }
             return acc; // ← return the accumulator to use for the next round
         }, { DEPOSIT: 0, WITHDRAWAL: 0 });
+        
         groupedBalance[tokenSymbol] = {
             Balance: accumulated['DEPOSIT'] - accumulated['WITHDRAWAL']
         };
 
     }
+    return groupedBalance;
+
+
+}
+
+
+function GroupArrayAccordingToTokenAndDate(ExraxtedCsvData, Date, Token) {
+
+    let ArrayGroupedAccordingToTokenAndDate = [];
+    ArrayGroupedAccordingToTokenAndDate = ExraxtedCsvData.reduce((acc, curr) => {
+
+        if((ConvertedCsvTimeStampToDate(curr[0]) == NormalisedDateParameter(Date))){ //Check if Dates Match then group by Token
+           
+            if(curr[2] == Token){
+                if (!acc[curr[2]]) {
+                    acc[curr[2]] = [];
+                }else{
+                        acc[curr[2]].push(curr);
+                }
+            }
+        }
+
+        return acc;
+
+    }, []);
+    
+   
+    return ArrayGroupedAccordingToTokenAndDate;
 }
 
 function GroupArrayAccordingToToken(ExraxtedCsvData, token=null) {
+    let ArrayGroupedAccordingToToken= [];
 
     if(token == null){//// No Token was provided, Filter with the Symbols that are in the CSV
-        groupedResult = ExraxtedCsvData.reduce((acc, curr) => {
+        ArrayGroupedAccordingToToken = ExraxtedCsvData.reduce((acc, curr) => {
             if (!acc[curr[2]]) {
                 acc[curr[2]] = [];
             }
@@ -126,48 +184,54 @@ function GroupArrayAccordingToToken(ExraxtedCsvData, token=null) {
             return acc;
         }, []);
     }else{
-        groupedResult = ExraxtedCsvData.reduce((acc, curr) => {
-            if (!acc[token]) {
-                acc[token] = [];
+        ArrayGroupedAccordingToToken = ExraxtedCsvData.reduce((acc, curr) => {
+
+            if(curr[2] == token){
+                if (!acc[curr[2]]) {
+                    acc[curr[2]] = [];
+                }
+                acc[curr[2]].push(curr);
             }
-            acc[token].push(curr);
             return acc;
         }, []);
     }
    
-    return groupedResult;
+    return ArrayGroupedAccordingToToken;
 }
 
 function GroupArrayAccordingToDate(ExraxtedCsvData, paramDate) {
 
-    groupedResult = ExraxtedCsvData.reduce((acc, curr) => {
+    let ArrayGroupedAccordingToDate = [];
+    ArrayGroupedAccordingToDate = ExraxtedCsvData.reduce((acc, curr) => {
 
-        if(ConvertedCsvTimeStampToDate(curr[0]) == NormalisedDateParameter(paramDate)){ //Group by Token
+        if((ConvertedCsvTimeStampToDate(curr[0]) == NormalisedDateParameter(paramDate))){ //Check if Dates Match
+           
             if (!acc[curr[2]]) {
                 acc[curr[2]] = [];
+            }else{
+                    acc[curr[2]].push(curr);
             }
-            acc[curr[2]].push(curr);
-            return acc;
+            
         }
-
-        
+        return acc;
     }, []);
-       
-    return groupedResult;
 
-    function ConvertedCsvTimeStampToDate(TimeStampFromCsv) {
-        let date = new Date(TimeStampFromCsv * 1000);
-        let dateFromCSV = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
-        return dateFromCSV;
-    }
 
-    function NormalisedDateParameter(paramDate) {
-        let dateFromParameter = new Date(paramDate);
-        let normalisedDate = dateFromParameter.getDate() + '/' + (dateFromParameter.getMonth() + 1) + '/' + dateFromParameter.getFullYear();
-        // console.log(dateFromParameter);
+    return ArrayGroupedAccordingToDate;
 
-        return normalisedDate;
-    }
+}
+function ConvertedCsvTimeStampToDate(TimeStampFromCsv) {
+    let date = new Date(TimeStampFromCsv * 1000);
+    let dateFromCSV = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+    return dateFromCSV;
+}
+
+function NormalisedDateParameter(paramDate) {
+    let dateFromParameter = new Date(paramDate);
+    let normalisedDate = dateFromParameter.getDate() + '/' + (dateFromParameter.getMonth() + 1) + '/' + dateFromParameter.getFullYear();
+    // console.log(dateFromParameter);
+
+    return normalisedDate;
 }
 // console.log('sdfgh');
 
